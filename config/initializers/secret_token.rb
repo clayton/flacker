5f9ac61be5621edca2f0b24a572b979a2fc2814c
Clayton Lengel-Zigich <Clayton Lengel-Zigich clayton@claytonlz.com>
2011-04-01 20:23:31 -0700

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flacker::Application.config.secret_token = '85d3426478e2da862818c5fb27dcdf1b406bdbde4644817ffe5f031cdf99fbf89d842a77b78b0beecbb362ee9ab181a41135ac40788fe0e1979d530577ce593d'
